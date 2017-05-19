070bb9028dde1be4d46081dc5ba62fb0ff86040e
thomas95 <jaredthomas68@gmail.com>
2017-05-19 17:13:29 -0600

    removed rand calls left in by accident

--- fd_vs_ex_grads_scaled.py
+++ fd_vs_ex_grads_scaled.py
@@ -12,7 +12,7 @@ from wakeexchange.larsen import larsen_wrapper, add_larsen_params_IndepVarComps
 # from wakeexchange.jensen import jensen_wrapper, add_jensen_params_IndepVarComps
 
 
-def plot_results(data, save=False, nTurbines=None, maxDirections=None):
+def plot_results(data, save=False, nTurbines=None, maxDirections=None, show=True):
 
     time_ex_f = data[0, :]
     time_ex_g = data[1, :]
@@ -40,13 +40,22 @@ def plot_results(data, save=False, nTurbines=None, maxDirections=None):
     plt.tight_layout()
     if save:
         plt.savefig('scaling_by_directions_combined_%i_turbines_%i_directions.pdf' % (nTurbines, maxDirections))
-    plt.show()
+
+    if show:
+        plt.show()
 
 
 def calc_data(nRows, maxDirections, savetxt=False):
 
-    # Scaling grid case
+    # set up wind farm
     rotor_diameter = 126.4
+    axialInduction = 1.0 / 3.0  # used only for initialization
+    generatorEfficiency = 0.944
+    hub_height = 90.0
+    Ct = 4.0 * axialInduction * (1.0 - axialInduction)
+    Cp = 0.7737 / 0.944 * 4.0 * 1.0 / 3.0 * np.power((1 - 1.0 / 3.0), 2)
+
+    # Scaling grid case
     spacing = 4  # turbine grid spacing in diameters
     nTurbines = nRows ** 2
 
@@ -59,20 +68,20 @@ def calc_data(nRows, maxDirections, savetxt=False):
     print turbineX, turbineY
 
     # initialize input variable arrays
-    rotorDiameter = np.ones(nTurbines) * np.random.random() * rotor_diameter
-    axialInduction = np.ones(nTurbines) * np.random.random() * (1. / 3.)
-    Ct = np.ones(nTurbines) * np.random.random()
-    Cp = np.ones(nTurbines) * np.random.random()
-    generatorEfficiency = np.ones(nTurbines) * np.random.random()
+    rotorDiameter = np.ones(nTurbines) * rotor_diameter
+    axialInduction = np.ones(nTurbines) * axialInduction
+    Ct = np.ones(nTurbines) * Ct
+    Cp = np.ones(nTurbines) * Cp
+    generatorEfficiency = np.ones(nTurbines) * generatorEfficiency
     yaw = np.zeros(nTurbines)
+    hubHeight = np.ones(nTurbines) * hub_height
 
     # Define flow properties
     wind_speed = 8.0  # m/s
     air_density = 1.1716  # kg/m^3
 
-    spacing = 5  # turbine grid spacing in diameters
-
-    directions = np.arange(1, maxDirections + 1, 20)
+    # initialize results arrays
+    directions = np.arange(1, maxDirections + 2, 10)
     time_ex_f = np.zeros_like(directions) * 0.0
     time_ex_g = np.zeros_like(directions) * 0.0
     time_fd_f = np.zeros_like(directions) * 0.0
@@ -104,7 +113,7 @@ def calc_data(nRows, maxDirections, savetxt=False):
         # assign values to constant inputs (not design variables)
         probf['turbineX'] = probg['turbineX'] = turbineX
         probf['turbineY'] = probg['turbineY'] = turbineY
-        probf['hubHeight'] = probg['hubHeight'] = np.zeros_like(turbineX) + 90.
+        probf['hubHeight'] = probg['hubHeight'] = hubHeight
         probf['yaw0'] = probg['yaw0'] = yaw
         probf['rotorDiameter'] = probg['rotorDiameter'] = rotorDiameter
         probf['axialInduction'] = probg['axialInduction'] = axialInduction
@@ -172,38 +181,8 @@ def N2_diagram(nRows, maxDirections):
     from openmdao.api import view_tree
 
     # Scaling grid case
-    rotor_diameter = 126.4
-    spacing = 4  # turbine grid spacing in diameters
     nTurbines = nRows ** 2
 
-    # Set up position arrays
-    points = np.linspace(start=spacing * rotor_diameter, stop=nRows * spacing * rotor_diameter, num=nRows)
-    xpoints, ypoints = np.meshgrid(points, points)
-    turbineX = np.ndarray.flatten(xpoints)
-    turbineY = np.ndarray.flatten(ypoints)
-
-    # initialize input variable arrays
-    rotorDiameter = np.ones(nTurbines) * np.random.random() * rotor_diameter
-    axialInduction = np.ones(nTurbines) * np.random.random() * (1. / 3.)
-    Ct = np.ones(nTurbines) * np.random.random()
-    Cp = np.ones(nTurbines) * np.random.random()
-    generatorEfficiency = np.ones(nTurbines) * np.random.random()
-    yaw = np.zeros(nTurbines)
-
-    # Define flow properties
-    wind_speed = 8.0  # m/s
-    air_density = 1.1716  # kg/m^3
-
-    spacing = 5  # turbine grid spacing in diameters
-
-    directions = np.arange(5, maxDirections + 1, 5)
-    time_ex_f = np.zeros_like(directions)
-    time_ex_g = np.zeros_like(directions)
-    time_fd_f = np.zeros_like(directions)
-    time_fd_g = np.zeros_like(directions)
-    AEPf = np.zeros_like(directions)
-    AEPg = np.zeros_like(directions)
-    index = 0
 
     probg = Problem(root=OptAEP(nTurbines, nDirections=maxDirections, use_rotor_components=False,
                                 wake_model=gauss_wrapper,
@@ -228,13 +207,20 @@ def N2_diagram(nRows, maxDirections):
 if __name__ == '__main__':
 
     # select how large of a wind farm and how many directions to run
-    nRows = 2 # wind farm will be of size nTurbines=nRows**2
+    nRows = 3 # wind farm will be of size nTurbines=nRows**2
     maxDirections = 100 # will start with 1 wind direction and run up to this number
 
-    data = calc_data(nRows, maxDirections, savetxt=False)
+    data = calc_data(nRows, maxDirections, savetxt=True)
 
     # data = load_data(nRows, maxDirections)
 
-    plot_results(data, save=False, nTurbines=nRows**2, maxDirections=maxDirections)
+    plot_results(data, save=True, nTurbines=nRows ** 2, maxDirections=maxDirections, show=True)
+
+    # for nRows in np.array([2, 4, 6, 8, 10, 12]):
+    #     data = calc_data(nRows, maxDirections, savetxt=True)
+    #
+    #     # data = load_data(nRows, maxDirections)
+    #
+    #     plot_results(data, save=True, nTurbines=nRows**2, maxDirections=maxDirections, show=False)
 
     # N2_diagram(nRows, maxDirections)
